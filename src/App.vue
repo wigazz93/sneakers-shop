b6479c6c88de6b2130dd92474d8b1f32f428a6f4
eugenio <wigazz93@mail.ru>
2024-02-09 14:30:50 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Hello from '@/components/Hello.vue'
+</script>
+
+<template>
+  <header class="mb-40 ml-50">
+    <Hello />
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="shadow-xl text-3xl font-bold underline color-red">Hello</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss"></style>
